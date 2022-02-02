db1fbaccf53e83f95600989639affba6115b226e
Vitaly <shmal9lk@gmail.com>
2022-02-02 22:30:16 +0500

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -131,8 +131,8 @@ const reload = (done) => {
 const watcher = () => {
   gulp.watch('source/less/**/*.less', gulp.series(styles));
   gulp.watch('source/js/*.js', gulp.series(scripts));
-  gulp.watch('source/*.html', gulp.series(html, reload));
-  // gulp.watch('source/*.html').on('change', browser.reload);
+  gulp.watch('source/*.html', gulp.series(html));
+  gulp.watch('source/img/**/*.svg', gulp.series(sprite, svg, reload));
 }
 
 //Build
